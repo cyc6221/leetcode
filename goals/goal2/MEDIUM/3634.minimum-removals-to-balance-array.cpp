952b36628b7f1a1d977c0b67d0d6094b824673c2
CYC.. <s24343162@gmail.com>
2025-08-16 21:51:39 +0800

    3634 update

--- goals/goal2/MEDIUM/3634.minimum-removals-to-balance-array.cpp
+++ goals/goal2/MEDIUM/3634.minimum-removals-to-balance-array.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int res = INT_MAX;
+    using ll = long long;
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-        int m = landStartTime.size();
-        int n = waterStartTime.size();
+        int res = 1;
+        int l = 0, r = 0;
 
-        for(int i=0; i<m; i++) {
-            int land = landStartTime[i] + landDuration[i];
-            for(int j=0; j<n; j++) {
-                if(land <= waterStartTime[j]) {
-                    res = min(res, waterStartTime[j] + waterDuration[j]);
-                }
-                else {
-                    res = min(res, land + waterDuration[j]);
-                }
-            }
+        for(l=0; l<n; l++) {
+            while(r < n && (ll)nums[r] <= (ll)nums[l] * k) r++;
+            res = max(res, r - l);
         }
-
-        for(int j=0; j<n; j++) {
-            int water = waterStartTime[j] + waterDuration[j];
-            for(int i=0; i<m; i++) {
-                if(water <= landStartTime[i]) {
-                    res = min(res, landStartTime[i] + landDuration[i]);
-                }
-                else {
-                    res = min(res, water + landDuration[i]);
-                }
-            }
-        }
-
-        return res;
+        return n - res;
     }
 };
